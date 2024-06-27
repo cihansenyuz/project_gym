13d90f491f3ae512627e1de2cea818e106d1c123
cihansenyuz <cihansenyuz@hotmail.com>
2024-06-27 16:09:27 +0300

    edit Exercise class

--- desktop_application/inc/exercise.h
+++ desktop_application/inc/exercise.h
@@ -17,10 +17,7 @@
 
 #include <QJsonObject>
 
-namespace Exercise
-{
-
-enum ExerciseName{
+/*enum ExerciseName{
     unknownName,
     benchPress,
     dumbbellInclinePress,
@@ -28,42 +25,17 @@ enum ExerciseName{
     barbellRow,
     overheadPress,
     dumbbellLateralRaise
-};
+};*/
+
 
 enum ExerciseType{
     unknownType,
     cardio,
-    shoulder,
-    chest,
-    back,
-    arm,
-    leg
+    strength,
+    strecting
 };
 
-inline QString ToString(const ExerciseType &type){
-    switch(type) {
-    case cardio: return "cardio";
-    case shoulder: return "shoulder";
-    case chest: return "chest";
-    case back: return "back";
-    case arm: return "arm";
-    case leg: return "leg";
-    default: return "unknownType";
-    }
-}
-
-inline ExerciseType fromStringExerciseType(const QString &str) {
-    if (str == "cardio") return cardio;
-    else if (str == "shoulder") return shoulder;
-    else if (str == "chest") return chest;
-    else if (str == "back") return back;
-    else if (str == "arm") return arm;
-    else if (str == "leg") return leg;
-    else return unknownType;
-}
-
-
-inline QString ToString(const ExerciseName &name){
+/*inline QString ToString(const ExerciseName &name){
     switch(name) {
     case benchPress: return "Bench Press";
     case dumbbellInclinePress: return "Dumbbell Incline Press";
@@ -83,36 +55,21 @@ inline ExerciseName fromStringExerciseName(const QString &str) {
     else if (str == "Overhead Press") return overheadPress;
     else if (str == "Dumbbell Lateral Raise") return dumbbellLateralRaise;
     else return unknownName;
-}
+}*/
+
 
 class Exercise{
 public:
     Exercise() = default;
-    Exercise(ExerciseType type, ExerciseName name, short set,
-             short repeat, short cooldown_period,
-             const QDate &last_done_date = QDate::currentDate());
-    void MarkExerciseDone();
-    bool IsReady();
-
     ExerciseType GetType() const;
-    ExerciseName GetName() const;
-    short GetSet() const;
-    short GetRepeat() const;
-    short GetCoolDownPeriod() const;
-    QDate GetLastDoneDate() const;
-
-    QJsonObject toJson() const;
+    virtual QJsonObject toJson() const = 0;
+    ExerciseType fromStringExerciseType(const QString &str);
+    QString ToString(const ExerciseType &type);
 
 private:
     ExerciseType type_;
-    ExerciseName name_;
-    short set_;
-    short repeat_;
-    short cooldown_period_;
-    QDate last_done_date_;
 };
 
-}
 
 
 
--- desktop_application/src/exercise.cpp
+++ desktop_application/src/exercise.cpp
@@ -1,39 +1,41 @@
 #include "../inc/exercise.h"
 
-namespace Exercise
-{
+ExerciseType Exercise::fromStringExerciseType(const QString &str) {
+    if (str == "cardio") return cardio;
+    else if (str == "shoulder") return shoulder;
+    else if (str == "chest") return chest;
+    else if (str == "back") return back;
+    else if (str == "arm") return arm;
+    else if (str == "leg") return leg;
+    else return unknownType;
+}
 
-Exercise::Exercise(ExerciseType type, ExerciseName name, short set,
-                   short repeat, short cooldown_period, const QDate &last_done_date)
-    : type_(type), name_(name), set_(set), repeat_(repeat),
-    cooldown_period_(cooldown_period), last_done_date_(last_done_date) {}
+QString Exercise::ToString(const ExerciseType &type){
+    switch(type) {
+    case cardio: return "cardio";
+    case shoulder: return "shoulder";
+    case chest: return "chest";
+    case back: return "back";
+    case arm: return "arm";
+    case leg: return "leg";
+    default: return "unknownType";
+    }
+}
 
-void Exercise::MarkExerciseDone(){ last_done_date_ = QDate::currentDate(); }
+/*void Exercise::MarkExerciseDone(){ last_done_date_ = QDate::currentDate(); }
 
 bool Exercise::IsReady(){
     if(QDate::currentDate() > last_done_date_.addDays(cooldown_period_))
         return true;
     else
         return false;
-}
+}*/
 
 ExerciseType Exercise::GetType() const { return type_; }
-ExerciseName Exercise::GetName() const { return name_; }
+/*ExerciseName Exercise::GetName() const { return name_; }
 short Exercise::GetSet() const { return set_; }
 short Exercise::GetRepeat() const { return repeat_; }
 short Exercise::GetCoolDownPeriod() const { return cooldown_period_; }
-QDate Exercise::GetLastDoneDate() const { return last_done_date_; }
+QDate Exercise::GetLastDoneDate() const { return last_done_date_; }*/
 
-QJsonObject Exercise::toJson() const {
-    QJsonObject json;
-    json["type"] = ToString(type_);
-    json["name"] = ToString(name_);
-    json["set"] = set_;
-    json["repeat"] = repeat_;
-    json["cooldown_period"] = cooldown_period_;
-    json["last_done_date"] = last_done_date_.toString(Qt::ISODate);
-    return json;
-}
-
-}
 
